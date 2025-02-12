108d6de3d9696d68e967d5fca06db3d338a50849
wendellast <wendelalves01a@hotmail.com>
2025-02-12 14:08:58 -0300

    initial project

---
+++ potato/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { cn } from '@/lib/utils'
+
+import "./globals.css";
+
+const fontSans = Plus_Jakarta_Sans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ['300','400','500', '600', '700']
+});
+
+
+
+export const metadata: Metadata = {
+  title: "Potato App",
+  description: "....",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable )} > {children}
+      </body>
+    </html>
+  );
+}
---
+++ potato/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl underline" >Heelo ok?</h1>
+      <Button>Clic me</Button>
+    </div>
+  )
+}
